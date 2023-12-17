6e8b5e80f3ffa9176d3d5737788309ae8fccb2d3
Arlettebrook <arlettebrook@foxmail.com>
2023-12-17 17:44:05 +0800

    test

--- kv.js
+++ kv.js
@@ -6,10 +6,16 @@ export async function onRequest(context) {
     const value = await env.kv_demo.get("dog");
     const value2 = await env.kv_demo.get('cat')
     const value3 = await env.kv_demo.list();
+    let num=''
+    for(let item of value3.keys){
+      console.log(item)
+      num+=item.name+':'+await env.kv_demo.get(item.name)
+    }
+    console.log(value3)
     if (value === null) {
       return new Response("Value not found", { status: 404 });
     }
-    return new Response(value+'@'+value2+'@'+value3);
+    return new Response(value+'@   '+value2+'   @'+num);
   } catch (err) {
     // In a production application, you could instead choose to retry your KV
     // read or fall back to a default code path.
